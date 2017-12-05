dcdabd327edf3764e6bbc6e47c79a841bde9f729
BogdanB01 <ioan.boca@info.uaic.ro>
2017-12-06 00:52:05 +0200

    Added User class

---
+++ eventis-solution/Data.Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Password { get; set; }
+
+        public virtual List<CustomEvent> CustomEvents;
+
+    }
+}
