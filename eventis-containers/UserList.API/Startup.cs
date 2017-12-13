9676b31dea78c464e40f89c6ac4a6d5deaf12981
Razvan Romanescu <romanescu.raz@gmail.com>
2017-12-13 08:39:48 +0200

    Versiune de demo

--- eventis-containers/UserList.API/Startup.cs
+++ eventis-containers/UserList.API/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Business;
+﻿using Business;
 using Data.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace UserList.API
