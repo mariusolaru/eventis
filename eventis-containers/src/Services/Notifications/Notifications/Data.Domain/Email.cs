1375a4f27f6f9c7fc51854a1a24214f484a312a9
mariusolaru <mariusolaru1997@yahoo.com>
2018-01-14 16:59:11 +0200

    Implemented Notifications API to send notifs via Email

---
+++ eventis-containers/src/Services/Notifications/Notifications/Data.Domain/Email.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+namespace Data.Domain
+{
+    public class Email
+    {
+        private Email()
+        {
+
+        }
+
+        public const string APIKey = "8f98d225-9003-4484-96d9-5e897fb145f7";
+
+        public const string Address = "https://api.elasticemail.com/v2/email/send";
+
+        public const string From = "eventis.noreply@gmail.com";
+
+        public const string FromName = "EventIS Team";
+
+        public static string WelcomeHtmlBody = System.IO.File.ReadAllText(@"C:\Users\mariu\Desktop\.NET Laborators\Proiect .NET\eventis\eventis-containers\src\Services\Notifications\Notifications\Data.Domain\Resources\WelcomeHTMLTemplate.txt");
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string BodyText { get; set; }
+
+        public static string Send(string address, NameValueCollection values)
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    byte[] apiResponse = client.UploadValues(address, values);
+                    return Encoding.UTF8.GetString(apiResponse);
+
+                }
+                catch (Exception ex)
+                {
+                    return "Exception caught: " + ex.Message + "\n" + ex.StackTrace;
+                }
+            }
+        }
+    }
+}
---
+++ eventis-containers/src/Services/Notifications/Notifications/Notifications/Controllers/NotificationsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Specialized;
+using Data.Domain;
+using ElasticEmail.WebApiClient;
+using Microsoft.AspNetCore.Mvc;
+using Notifications.Models;
+
+namespace Notifications.Controllers
+{
+    [Route("api/notifications")]
+    public class NotificationsController
+    {
+        [HttpPost("welcome")]
+        public void SendWelcomeEmail([FromBody] EmailAttrModel emailAttr)
+        {
+            NameValueCollection values = new NameValueCollection();
+            values.Add("apikey", Email.APIKey);
+            values.Add("from", Email.From);
+            values.Add("fromName", Email.FromName);
+            values.Add("to", emailAttr.To);
+            values.Add("subject", "Welcome to EventIS");
+            values.Add("bodyText", "Text Body");
+            values.Add("bodyHtml", Email.WelcomeHtmlBody);
+            values.Add("isTransactional", "true");
+
+            string Response = Email.Send(Email.Address, values);
+
+        }
+
+    }
+}
